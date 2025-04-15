26ca7f58648c7b7f7e6c8380a5193b9e6ae18b50
PJA0103 <anne0103@outlook.com>
2025-04-15 14:32:00 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require ('express');
+const app = express();
+const bodyParser = require ('body-parser');
+const http = require ('http').Server(app);
+const io = require ('socket.io')(http);
+
+
+app.use(express.static(__dirname)); // 靜態檔 aka HTML
+app.use(express.json()); // JSON資料
+app.use(bodyParser.urlencoded({ extended: false})); // 表單資料
+
+const messages = [];
+
+app.get('/messages', (req, res) =>{
+    res.send(messages)
+  })
+
+app.post('/messages', (req, res) =>{
+    messages.unshift(req.body)
+    io.emit('message', req.body)
+    res.sendStatus(200)
+})
+io.on('connection', (socket) =>{
+    console.log('有人進來囉')
+})
+  
+const server = http.listen(3000,'127.0.0.1', ()=>{
+    console.log(`開始監聽`);
+});
